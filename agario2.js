6eadbd40b872f39e751ec7f94924b205aade4086
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2016-09-09 11:54:27 +0100

    Some testing

---
+++ agario2.js
@@ -0,0 +1,21 @@
+//link to  BG image
+var gridBG = 'pics/agarbg.png';
+
+//load background image before game starts
+function preload(){
+  bg = loadImage(gridBG);
+}
+
+
+
+//Setup runs once at the beginning
+function setup() {
+  createCanvas(500,500);
+  background(loadImage(gridBG));
+}
+
+//Draw runs forever 
+function draw() {
+ 
+  
+}
---
+++ sketch.js
@@ -0,0 +1,16 @@
+//link to  BG image
+var gridBG = 'pics/agarbg.png'
+
+
+//load background image before game starts
+function preload(){
+  bg = loadImage(gridBG)
+}
+
+function setup() {
+
+}
+
+function draw() {
+  ellipse(50, 50, 80, 80);
+}
